1e6440ef91d87fa68be711afd13d52b5d6c82d86
jramosf99 <jramosf99@gmail.com>
2021-03-31 14:39:52 +0200

    files with date and completed path in linux

--- code/files.py
+++ code/files.py
@@ -1,6 +1,8 @@
 import inotify.adapters
 import pandas as pd
+from datetime import datetime
 import os
+
 #tyoes of actions
 # 'IN_MOVED_TO' 
 # "IN_CLOSE_NOWRITE"  
@@ -13,7 +15,7 @@ import os
 # 'IN_MOVED_FROM'
 
 list = ['IN_MOVED_TO', "IN_DELETE" , 'IN_CREATE', 'IN_MODIFY', 'IN_CLOSE_WRITE','IN_MOVED_FROM']#actions to register
-columnsNames = ["path","file_name","actions"] #columns names of the CSV files
+columnsNames = ["path","actions","time"] #columns names of the CSV files
 
 def files(path1,path2):
     outputPath = path1#path of the CSV output file
@@ -23,7 +25,7 @@ def files(path1,path2):
         for event in i.event_gen(yield_nones=False):
             (_, type_names, path, filename) = event    
             if any(elem in list  for elem in type_names):
-                fileEvent = [path, filename, type_names]
+                fileEvent = [os.path.join(path, filename), type_names, datetime.now()]
                 df1 = pd.DataFrame([fileEvent], columns = columnsNames)
                 if not os.path.isfile(outputPath):
                     df1.to_csv(outputPath, index=None, header=True)
